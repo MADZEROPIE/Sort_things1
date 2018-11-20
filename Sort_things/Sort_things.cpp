2e163c297a6763cebd1c06232d8df85e49b19ea4
MADZEROPIE <41443900+MADZEROPIE@users.noreply.github.com>
2018-11-20 22:31:12 +0300

    Update Sort_things.cpp

--- Sort_things/Sort_things.cpp
+++ Sort_things/Sort_things.cpp
@@ -8,7 +8,7 @@
 
 
 
-#define N 10000
+#define N 200000
 #define K 12
 #define Q 12
 #define NUM 20
@@ -18,185 +18,191 @@ struct sorts_time
 	int time_sort;
 	char name[30];
 	int num;
+	int sorted_arr[NUM];
 };
-/*
-int choice(char menu[][Q],int n) {
-	int k=-1,i;
-	for (i = 0; i < n; i++)
-		printf("%s (%d)\n", menu[i],i);
-        while (k < 0 || k>10) {
-		printf("Please choose wisely: ");
-		scanf_s("%d", &k);
-	}
-	return k;	
-}
 
-int input(int arr[], int len) {
-	int i, n=-1;
+
+
+int NN;
+
+int* input(int *arr, int len) {
+	int i;
 	printf("Press 0 for default settings or any other number for choose new settings ");
 	scanf_s("%d", &i);
 	if (i == 0)
-		return len;
-	while (n < 0 || n>7777777) {
-		printf("Enter number of elements");
-		scanf_s("%d", &n);
+		return arr;
+	len = -1;
+	while (len < 0 || len>7777777) {
+		printf("Enter number of elements: ");
+		scanf_s("%d", &len);
 	}
-	arr = (int *)realloc(arr, n*sizeof(int));
+	NN = len;
+	//arr = (int *)realloc(arr, n*sizeof(int));
+	arr = (int *)malloc(len * sizeof(int));
 	printf("Press 0 if you wanna create random array or any other number if you wanna enter it mannualy ");
 	scanf_s("%d", &i);
-	if (i == 0)
-		create_r_arr(arr, n);
+	if (i == 0 || len > 10000)
+		create_r_arr(arr, len);
 	else
-		for (int j = 0; j < n;j++)
+		for (int j = 0; j < len; j++)
 			scanf_s("%d", &arr[j]);
-	return n;
-	
+	return arr;
+
 }
-
-void create_r_arr(int arr[], int n) {
-	for (int i = 0; i < n; i++)
-		arr[i] = rand();
-}
-
-*/
-
-int c_arr[N];
-//c_arr = (int*)malloc(N * sizeof(int));
 
 int main()
 {
-
-    int n=N;
-	int *arr=(int*)malloc(N*sizeof(int));    
-    char menu[Q][25] = {"Input","Output","Bubble sort","Shaker sort","Quick sort","Selection sort","Shell sort","Merge sort","Insertion sort (binary)","Insertion sort (linear)","Merge sort (down-top)","Exit"};
-	int k,flag=1,flag1=0;
-	sorts_time sorts_arr[K] = { {0," ",0} };
-    //int time_arr[Q];
-    int s_time,e_time,count=0;
+	int *c_arr;
+	int n = N;
+	int *arr = (int*)malloc(N * sizeof(int));
+	c_arr = (int*)malloc(N * sizeof(int));
+	char menu[Q][25] = { "Input","Output","Bubble sort","Shaker sort","Quick sort","Selection sort","Shell sort","Merge sort","Insertion sort (binary)","Insertion sort (linear)","Merge sort (down-top)","Exit" };
+	int k = 0, flag1 = 0;
+	sorts_time sorts_arr[K] = { {0," ",0,0} };
+	//int time_arr[Q];
+	int s_time, e_time, count = 0;
 	srand(time(0));
 	create_r_arr(c_arr, N);
-    FILE* file;
-    fopen_s(&file,"sorts_time.txt", "a");
-   /* if (file != NULL) {
-        fprintf(file, "\n%d \n", N);
-        for (int i = 0; i < K; i++)
-            fprintf(file, "%s \t\t %d\n", sorts_arr[i].name, sorts_arr[i].time_sort);
-                }
-*/
-	while (flag) {
-        k = choice(menu, 11);
-		
+	FILE* file;
+	fopen_s(&file, "sorts_time.cvs", "a");
+	/* if (file != NULL) {
+		 fprintf(file, "\n%d \n", N);
+		 for (int i = 0; i < K; i++)
+			 fprintf(file, "%s \t\t %d\n", sorts_arr[i].name, sorts_arr[i].time_sort);
+				 }
+ */
+	while (k != 11) {
+		k = choice(menu, Q);
+
 		switch (k)
 		{
 		case 0:
-			n = input(c_arr, N);
+			c_arr = input(c_arr, N);
 			free(arr);
-			arr = (int *)malloc(n*sizeof(c_arr[0]));
-			copy_arr(c_arr,arr,n);
+			n = NN;
+			arr = (int *)malloc(n * sizeof(c_arr[0]));
+			copy_arr(c_arr, arr, n);
 			break;
 
-        case 1:
-                printf("Default array :\n");
-                print_arr(c_arr,NUM);
-                printf("\nSorted array :\n");
-                print_arr(arr,NUM);
-				printf("\n\n");
-                if (file != NULL) {
-                	fprintf(file, "\n%d \n", n);
-                	if (flag1)
-                		for (int i = count; i < K; i++){
-                        	fprintf(file, "%d\t%s\t %d \n", sorts_arr[i].num, sorts_arr[i].name, sorts_arr[i].time_sort);
-                        	printf("%d\t%s\t %d \n",  sorts_arr[i].num, sorts_arr[i].name, sorts_arr[i].time_sort);}
-                    for (int i = 0; i < count; i++){
-                        	fprintf(file, "%d\t%s\t %d \n", sorts_arr[i].num, sorts_arr[i].name, sorts_arr[i].time_sort);
-                        	printf("%d\t%s\t %d \n", sorts_arr[i].num, sorts_arr[i].name, sorts_arr[i].time_sort);}
+		case 1:
+			printf("Default array :\n");
+			print_arr(c_arr, NUM);
+			//printf("\nSorted array :\n");
+			//print_arr(arr,NUM);
+			printf("\n\n");
+			if (file != NULL) {
+				//fprintf(file, "\n%d \n", n);
+				if (flag1)
+					for (int i = count; i < K; i++) {
+						fprintf(file, "%d,%s,%d \n", sorts_arr[i].num, sorts_arr[i].name, sorts_arr[i].time_sort);
+						printf("%d\t%s\t %d \n", sorts_arr[i].num, sorts_arr[i].name, sorts_arr[i].time_sort);
+						printf("\nSorted array :\n");
+						print_arr(sorts_arr[i].sorted_arr, NUM);
+						printf("\n");
+					}
+				for (int i = 0; i < count; i++) {
+					fprintf(file, "%d,%s, %d \n", sorts_arr[i].num, sorts_arr[i].name, sorts_arr[i].time_sort);
+					printf("%d\t%s\t %d \n", sorts_arr[i].num, sorts_arr[i].name, sorts_arr[i].time_sort);
+					printf("\nSorted array :\n");
+					print_arr(sorts_arr[i].sorted_arr, NUM);
+					printf("\n");
+				}
 
-                    }
-                 break;
+			}
+			break;
 
-        case 2:
+		case 2:
 			copy_arr(c_arr, arr, n);
-                s_time=clock();
-                bubble_sort(arr,n);
-            	e_time=clock();
-            	sorts_arr[count++]={e_time-s_time,"Bubble sort",n};
-                break;
+			s_time = clock();
+			bubble_sort(arr, n);
+			e_time = clock();
+			sorts_arr[count] = { e_time - s_time,"Bubble sort",n };
+			copy_arr(arr, sorts_arr[count++].sorted_arr, NUM);
+			break;
 
-        case 3:
+		case 3:
 			copy_arr(c_arr, arr, n);
-        	s_time=clock();
-            shaker_sort(arr,n);
-            e_time=clock();
-            sorts_arr[count++]={e_time-s_time,"Shaker sort",n };
-            break;
+			s_time = clock();
+			shaker_sort(arr, n);
+			e_time = clock();
+			sorts_arr[count] = { e_time - s_time,"Shaker sort",n };
+			copy_arr(arr, sorts_arr[count++].sorted_arr, NUM);
+			break;
 
-        case 4:
+		case 4:
 			copy_arr(c_arr, arr, n);
-        	s_time=clock();
-            quick_sort(arr,0,n);
-            e_time=clock();
-            sorts_arr[count++]={e_time-s_time,"Quick sort",n };
-            break;
+			s_time = clock();
+			quick_sort(arr, 0, n);
+			e_time = clock();
+			sorts_arr[count] = { e_time - s_time,"Quick sort",n };
+			copy_arr(arr, sorts_arr[count++].sorted_arr, NUM);
+			break;
 
-        case 5:
+		case 5:
 			copy_arr(c_arr, arr, n);
-        	s_time=clock();
-            selection_sort(arr,n);
-            e_time=clock();
-            sorts_arr[count++]={e_time-s_time,"Selection sort",n };
-            break;
+			s_time = clock();
+			selection_sort(arr, n);
+			e_time = clock();
+			sorts_arr[count] = { e_time - s_time,"Selection sort",n };
+			copy_arr(arr, sorts_arr[count++].sorted_arr, NUM);
+			break;
 
-        case 6:
+		case 6:
 			copy_arr(c_arr, arr, n);
-        	s_time=clock();
-            shaker_sort(arr,n);
-            e_time=clock();
-            sorts_arr[count++]={e_time-s_time,"Shell sort",n };
-            break;
+			s_time = clock();
+			shaker_sort(arr, n);
+			e_time = clock();
+			sorts_arr[count] = { e_time - s_time,"Shell sort",n };
+			copy_arr(arr, sorts_arr[count++].sorted_arr, NUM);
+			break;
 
-        case 7:
+		case 7:
 			copy_arr(c_arr, arr, n);
-        	s_time=clock();
-            merge_sort(arr, 0,n-1);
-            e_time=clock();
-            sorts_arr[count++]={e_time-s_time,"Merge sort",n };
-            break;
+			s_time = clock();
+			merge_sort(arr, 0, n - 1);
+			e_time = clock();
+			sorts_arr[count] = { e_time - s_time,"Merge sort",n };
+			copy_arr(arr, sorts_arr[count++].sorted_arr, NUM);
+			break;
 
 		case 8:
 			copy_arr(c_arr, arr, n);
-        	s_time=clock();
-            insertion_sort(arr,n);
-            e_time=clock();
-            sorts_arr[count++]={e_time-s_time,"Insertion sort (binary)",n };
-            break;
-
-        case 9:
-			copy_arr(c_arr, arr, n);
-        	s_time=clock();
-            insertion_sort(n,arr);
-            e_time=clock();
-            sorts_arr[count++]={e_time-s_time,"Insertion sort (linear)",n };
-            break;
-
-        case 10:
-        	copy_arr(c_arr, arr, n);
-        	s_time=clock();
-			merge_sort(arr,n);
-            e_time=clock();
-            sorts_arr[count++]={e_time-s_time,"Merge sort (down-top)",n };
-            break;
-
-		default:
-			flag = 0;
+			s_time = clock();
+			insertion_sort(arr, n);
+			e_time = clock();
+			sorts_arr[count] = { e_time - s_time,"Insertion sort (binary)",n };
+			copy_arr(arr, sorts_arr[count++].sorted_arr, NUM);
 			break;
+
+		case 9:
+			copy_arr(c_arr, arr, n);
+			s_time = clock();
+			insertion_sort(n, arr);
+			e_time = clock();
+			sorts_arr[count] = { e_time - s_time,"Insertion sort (linear)",n };
+			copy_arr(arr, sorts_arr[count++].sorted_arr, NUM);
+			break;
+
+		case 10:
+
+			copy_arr(c_arr, arr, n);
+			s_time = clock();
+			merge_sort(arr, n);
+			e_time = clock();
+			sorts_arr[count] = { e_time - s_time,"Merge sort (down-top)",n };
+			copy_arr(arr, sorts_arr[count++].sorted_arr, NUM);
+			break;
+
 		}
 
-		if (count==K){
-			count=count % K;
-			flag1=1;} 
+		if (count == K) {
+			count = count % K;
+			flag1 = 1;
 		}
+	}
 
-    fclose(file);
-    return 0;
+
+	fclose(file);
+	return 0;
 }
 
